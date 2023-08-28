6a82c3ae3a804a71126f72ea3ec35d661f2f2e3f
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2023-08-28 23:27:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+  
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Help Poor & Needy People....' )
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
